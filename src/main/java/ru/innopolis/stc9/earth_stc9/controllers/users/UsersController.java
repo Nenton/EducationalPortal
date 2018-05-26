3c1c91ca729c3169035ebb5a6115194314c11954
= <sergejsusev@yandex.ru>
2018-05-26 19:44:18 +0500

    ER-11 Перевести проект на Spring MVC Добавил файл main-servlet с конфигом viewResolver-a В файле web.xml добавил сервлет DispatcherServlet. В маппинге указал только /users.

--- src/main/java/ru/innopolis/stc9/earth_stc9/controllers/users/UsersController.java
+++ src/main/java/ru/innopolis/stc9/earth_stc9/controllers/users/UsersController.java
@@ -1,20 +1,32 @@
 package ru.innopolis.stc9.earth_stc9.controllers.users;
 
-import com.sun.istack.internal.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import ru.innopolis.stc9.earth_stc9.pojo.Role;
+import ru.innopolis.stc9.earth_stc9.pojo.User;
+import ru.innopolis.stc9.earth_stc9.services.IUsersService;
 
-import javax.servlet.annotation.WebServlet;
+import java.util.List;
 
-@WebServlet("/users")
-public class UsersController extends UsersAbstractController {
+@Controller
+public class UsersController {
+    @Autowired
+    private IUsersService service;
 
-    @Override
-    public int getRoleCreate() {
-        return 0;
-    }
-
-    @Override
-    @NotNull
-    public String getPathPage() {
-        return "/users";
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public String getStudents(Model model) {
+        List<User> users = service.getUsers();
+        List<Role> roles = service.getRoles();
+        if (users != null) {
+            model.addAttribute("users", users);
+        }
+        if (roles != null) {
+            model.addAttribute("roles", roles);
+        }
+        model.addAttribute("path", "/users");
+        return "users";
     }
 }
