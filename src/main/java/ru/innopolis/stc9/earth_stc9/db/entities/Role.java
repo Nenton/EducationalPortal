482075f0a689e337296969e902ec736bcb5af2cb
= <sergejsusev@yandex.ru>
2018-06-23 21:49:53 +0500

    feature: Добавил контекст и зависимости для hibernate
    feature: Добавил контекст в web.xml и изменил database.xml
    feature: Добавил Lombok и entities
    
    fixbug: Заработал Lombok
    
    В настройках надо было включить Annotation processor
    
    fixbug: Разрешал конфликты с гитом
    
    fixbug: Удалил файл с базой

---
+++ src/main/java/ru/innopolis/stc9/earth_stc9/db/entities/Role.java
@@ -0,0 +1,30 @@
+package ru.innopolis.stc9.earth_stc9.db.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Table(name = "roles")
+@Entity
+public class Role {
+    @Setter
+    private long id;
+    @Getter
+    @Setter
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @SequenceGenerator(name = "roleSeq", sequenceName = "ROLE_SEQUENCE")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "roleSeq")
+    public long getId() {
+        return id;
+    }
+}
